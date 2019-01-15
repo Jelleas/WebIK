ab53a9e5045aac4f4906c50fb537a0532d5d6a60
Roan Schellingerhout <roan.schellingerhout@student.uva.nl>
2019-01-15 11:20:58 +0000

    werkend met nieuwe database

--- helpers.py
+++ helpers.py
@@ -11,15 +11,14 @@ import ast
 
 db = SQL("sqlite:///geo_guru.db")
 
-def create_game(user_id, invite_id):
+def create_game(player1_id, player2_id):
 
     # haal vragen op
     vragen = requests.get("https://opentdb.com/api.php?amount=50&category=22&type=multiple")
     # zet ze in json
     json = vragen.json()
     # insert de benodigde gegevens in de database
-    db.execute("INSERT INTO games (user_id, invite_id, score, questions) VALUES (:user_id, :invite_id, :score, :questions)", user_id=user_id, invite_id=invite_id, score=0, questions=str(json))
-
+    db.execute("INSERT INTO games (player1_id, player2_id, score, questions) VALUES (:player1_id, :player2_id, :score, :questions)", player1_id=player1_id, player2_id=player2_id, score=0, questions=str(json))
 
 def login_required(f):
     """
